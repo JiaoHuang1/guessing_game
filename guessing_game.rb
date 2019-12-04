c0e3caf2127940ce5dd226ffc796efddde16e6d1
Jiao <jiaohuang2013@gmail.com>
2019-12-04 18:44:24 -0500

    initial commit

---
+++ guessing_game.rb
@@ -0,0 +1,31 @@
+class GuessingGame
+    attr_reader :num_attempts
+    def initialize(min, max)
+        @secret_num = rand(min..max)
+        @num_attempts = 0
+        @game_over = false
+    end
+
+    def game_over?
+        @game_over
+    end
+
+    def check_num(n)
+        @num_attempts += 1
+        if n == @secret_num
+            @game_over = true 
+            print "you win"
+        elsif n > @secret_num
+            print "too big"
+        else  
+            print "too small"
+        end
+    end
+
+    def ask_user
+        print "enter a number"
+        number = gets.chomp.to_i
+        self.check_num(number)
+    end
+
+end
